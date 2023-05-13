9b502dae0b82380af2689421a3f919247eec0e7f
LockG <locklingeorge@gmail.com>
2023-05-13 19:02:37 -0400

    improved day 4

--- 4/assignments.py
+++ 4/assignments.py
@@ -1,25 +1,18 @@
 assignments = open('test_data.txt','r')
 pairs = []
 OVpairs = []
+elfset = ['']*3
 for line in assignments:
     elf = line.strip('\n')
     elf = elf.split(',')
     for i in range(len(elf)):
         elf[i] = list(map(int,elf[i].split('-')))
-    elfset1 = set(range(elf[0][0],elf[0][1]+1))
-    if not elfset1:
-        elfset1 = set([elf[0][0]])
-    elfset2 = set(range(elf[1][0],elf[1][1]+1))
-    if not elfset2:
-        elfset2 = set([elf[1][0]])
-    #print(elfset1)
-    #print(elfset2)
-    
-    if len(elfset1) >= len(elfset2):
-        pairs.append(int(elfset1.issuperset(elfset2)))
+        elfset[i] = set(range(elf[i][0],elf[i][1]+1))
+    if len(elfset[0]) >= len(elfset[1]):
+        pairs.append(int(elfset[0].issuperset(elfset[1])))
     else:
-        pairs.append(int(elfset1.issubset(elfset2)))
-    OVpairs.append(int(not elfset1.isdisjoint(elfset2)))
+        pairs.append(int(elfset[0].issubset(elfset[1])))
+    OVpairs.append(int(not elfset[0].isdisjoint(elfset[1])))
 print(sum(pairs))
 print(sum(OVpairs))
 assignments.close()
